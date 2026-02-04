dda9bab0c65ef3a5b04edd3822553d7d86d7c36f
utblr <ufuktan@utblr.xyz>
2026-02-04 16:19:44 +0300

    add failure message

--- tests/test_suite.m
+++ tests/test_suite.m
@@ -12,7 +12,11 @@ try
     endfor
     disp('TEST_PASSED');
     exit(0);
-catch
+catch ME
+    fprintf(stderr(), "\n--- TEST FAILURE ---\n");
+    fprintf(stderr(), "Message: %s\n", ME.message);
+    fprintf(stderr(), "Location: %s (Line %d)\n", ME.stack(1).name, ME.stack(1).line);
+    fprintf(stderr(), "--------------------\n");
     disp('TEST_FAILED');
     exit(1); % Exit with error code so GitHub sees the failure
 end
